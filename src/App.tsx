a4cb44a6df4a3da787497bcbadc5cba9ffa52c89
chystarr <chynnastarr2@gmail.com>
2023-10-31 23:51:33 -0400

    Configure React + TypeScript project with Vite and set up Tailwind and shadcn UI components

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import './App.css'
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+
+function App() {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <>
+      <Button onClick={() => setCount(count + 1)}>count is {count}</Button>
+    </>
+  );
+}
+
+export default App;
