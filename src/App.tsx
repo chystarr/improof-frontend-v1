c3814a3b072a09500d78a370c78b469017c13158
chystarr <chynnastarr2@gmail.com>
2023-11-02 12:33:24 -0400

    Set up shadcn UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-// import './App.css'
+import "./App.css";
 import { useState } from "react";
 
-import { Button } from "@/components/ui/button";
+import Button from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState<number>(0);
